ba91e66f14b1b97467c61d92e807d0c1f49fd187
sumon2807 <sharifulbd911@gmail.com>
2023-05-02 13:19:19 +0600

    responsive  navigation bar is ready for UI which is include a signin button a user profile icon and a website logo

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home page</h2>
+        <div >
+            <Carousel fade>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="../../../../public/assets/images/slide-4.jpg"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="../../../../public/assets/images/slide-5.jpg"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="../../../../public/assets/images/slide-6.jpg"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Badge, Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is header</h2>
+            <Navbar collapseOnSelect expand="lg" className='position-absolute fixed-top' variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src="../../../../public/assets/images/logo.png" alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto fs-5 fw-bold text-white">
+                            <Nav.Link href="#features">HOME</Nav.Link>
+                            <Nav.Link href="#pricing">OUR MANU</Nav.Link>
+                            <Nav.Link href="#pricing">SHOP</Nav.Link>
+                            <Nav.Link href="#pricing">BLOG</Nav.Link>
+                            <Nav.Link href="#pricing">CONTACT</Nav.Link>
+                        </Nav>
+                        <Nav className=''>
+                            <Nav.Link href="#deets" className='fs-3'><FaUserCircle /></Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                <Button variant="outline-dark" className="fs-5 fw-bold">SIGN IN</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
