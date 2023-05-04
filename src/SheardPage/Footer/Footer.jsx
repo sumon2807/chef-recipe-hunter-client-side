9d5843a592a0019782c1f21fca62e2bbe2b18f55
sumon2807 <sharifulbd911@gmail.com>
2023-05-04 16:51:31 +0600

    footer page design is ready for UI and a navbar is added also

--- src/SheardPage/Footer/Footer.jsx
+++ src/SheardPage/Footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaPinterestP } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer pageeeeeeeeeee</h1>
+        <div className='lg:my-8 text-center'>
+            <div className='flex justify-center'>
+                <img src="../../../public/assets/images/logo.png" alt="" />
+            </div>
+            <div className='flex justify-center gap-8 mt-4'>
+                <FaFacebookF className='text-3xl hover:text-rose-700' />
+                <FaTwitter className='text-3xl hover:text-rose-700' />
+                <FaInstagram className='text-3xl hover:text-rose-700' />
+                <FaYoutube className='text-3xl hover:text-rose-700' />
+                <FaPinterestP className='text-3xl hover:text-rose-700' />
+            </div>
+            <div className="flex justify-center gap-4 mt-6">
+                <a className="link link-hover hover:text-rose-700 font-bold">ABOUT US |</a>
+                <a className="link link-hover hover:text-rose-700 font-bold">MANU |</a>
+                <Link to="/order"><p className="link link-hover hover:text-rose-700 font-bold">ORDER NOW |</p></Link>
+                <a className="link link-hover hover:text-rose-700 font-bold">GIFT CARDS |</a>
+                <a className="link link-hover hover:text-rose-700 font-bold">CONTACTS</a>
+            </div>
+            <h2 className='pt-4'>© 2023 Testo. All Rights Reserved</h2>
         </div>
     );
 };
